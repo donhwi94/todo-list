07b5d061d48009b619cd06b0e6492cf2b8ad6cbb
donhwi94 <ehsgnl123@naver.com>
2021-04-28 22:08:24 +0900

    first commit

---
+++ todo/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    # id = models.AutoField() # 자동 생성
+    title = models.TextField(blank=False)
+    description = models.TextField(blank=False)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+class Comment(models.Model):
+    # id = models.AutoField() # 자동 생성
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    contents = models.TextField(blank=False)
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now_add=True)
