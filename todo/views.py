416a0b668f42120a1b266c7368f7c74300643e82
donhwi94 <ehsgnl123@naver.com>
2021-04-29 12:53:42 +0900

    views 코드 리팩토링 - generic CBV 사용

--- todo/views.py
+++ todo/views.py
@@ -1,28 +1,12 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 from .models import Todo, Comment
 from .serializers import TodoSerializer, CommentSerializer
 
-class TodoList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class TodoList(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class TodoDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
