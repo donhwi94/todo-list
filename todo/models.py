cffd9224017d41f4ec7799c497023f194f8c8b96
donhwi94 <ehsgnl123@naver.com>
2021-05-03 23:27:07 +0900

    models 필드 옵션 수정

--- todo/models.py
+++ todo/models.py
@@ -5,14 +5,14 @@ class Todo(models.Model):
     title = models.TextField(blank=False)
     description = models.TextField(blank=False)
     is_completed = models.BooleanField(default=False)
-    created_at = models.DateField(auto_now=True)
-    updated_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
 class Comment(models.Model):
     # id = models.AutoField() # 자동 생성
     todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
     contents = models.TextField(blank=False)
-    created_at = models.DateField(auto_now=True)
-    updated_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     
